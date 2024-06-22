0c9f5d00e143efca9547ba92f63078d0ee04593b
Souzai83 <chikushi3.227@gmail.com>
2024-06-22 15:35:04 +1000

    OPP: added method in Person class

--- OOP/class.ts
+++ OOP/class.ts
@@ -1,10 +1,18 @@
 class Person {
+    //property
     name: string;
 
     constructor(initName: string) {
         this.name = initName;
     }
+
+    //method
+    greeting() {
+        console.log(`Hello. My name is ${this.name}`);
+    }
+
 }
 
 const Shiori = new Person("Shiori");
+Shiori.greeting();
 console.log('Shiori');
--- dist/OOP/class.js
+++ dist/OOP/class.js
@@ -3,6 +3,11 @@ class Person {
     constructor(initName) {
         this.name = initName;
     }
+    //method
+    greeting() {
+        console.log(`Hello. My name is ${this.name}`);
+    }
 }
 const Shiori = new Person("Shiori");
+Shiori.greeting();
 console.log('Shiori');
