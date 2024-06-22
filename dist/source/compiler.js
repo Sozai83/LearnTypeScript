b2f1bcfc0f294a04c80accf6cd7d96c8f363672a
Souzai83 <chikushi3.227@gmail.com>
2024-06-22 13:29:24 +1000

    noImplicitAny

---
+++ dist/source/compiler.js
@@ -0,0 +1,8 @@
+"use strict";
+let hello = "hello";
+//hi
+console.log(hello.toUpperCase());
+
+function hi(message){
+    console.log(message);
+}
--- source/compiler.ts
+++ source/compiler.ts
@@ -1,3 +1,13 @@
-let hello = 32;
+let hello = "hello";
 //hi
 console.log(hello.toUpperCase());
+
+function hi(message: string) {
+    console.log(message);
+}
+
+let implicitAny;
+//implicityAny : any
+implicitAny = "string";
+//implicityAny : string
+implicitAny.toUpperCase();
